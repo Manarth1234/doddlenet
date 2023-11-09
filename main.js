571b3cf3deba1aaeab65336064e1deabde67794a
Manarth1234 <129154269+Manarth1234@users.noreply.github.com>
2023-11-09 18:30:41 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// array_1 = ["pen","paper","book","bottle"];
+// random_number = Math.floor((Math.random() * array_1.length) + 1);
+// console.log(array_1[random_number]);
+// random_sketch_name = array_1[random_number]; 
+
+quick_draw_data_set=["aircraft carrier","airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball bat","basket","basketball","bat","bathtub","beach","bear","beard","bed","bee","belt","bench","bicycle","binoculars","bird","birthday cake","blackberry","blueberry","book","boomerang","bottlecap","bowtie","bracelet","brain","bread","bridge","broccoli","broom","bucket","bulldozer","bus","bush","butterfly","cactus","cake","calculator","calendar","camel","camera","camouflage","campfire","candle","cannon","canoe","car","carrot","castle","cat","ceiling fan","cello","cell phone","chair","chandelier","church","circle","clarinet","clock","cloud","coffee cup","compass","computer","cookie","cooler","couch","cow","crab","crayon","crocodile","crown","cruise ship","cup","diamond","dishwasher","diving board","dog","dolphin","donut","door","dragon","dresser","drill","drums","duck","dumbbell","ear", "elbow","elephant"]
+random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+console.log(quick_draw_data_set[random_number]);
